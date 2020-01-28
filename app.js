614b1077d7e28f63628054231c56b27e9abb3b6a
unknown <nikhil.gupta@veterandevelopers.com>
2020-01-28 18:46:07 +0530

    updated

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(err, req, res) {
 
   logger.error(err);
 
-  // set locals, only providing error in development
+  // set locals,  only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
